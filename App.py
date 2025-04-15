35624526902fc1e08dc01a13e69f36c2e7a69dec
Nivin Benny <134007084+Nivin24@users.noreply.github.com>
2025-04-15 09:54:58 +0530

    Update App.py

--- App.py
+++ App.py
@@ -32,5 +32,10 @@ def topic_page(name):
         return render_template('404.html'), 404
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#changed this code    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
